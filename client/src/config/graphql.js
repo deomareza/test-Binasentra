7eb19d4f965353c3b0c7d12aa9b0ff8588453ef5
Deo Ivan Mareza <deomareza@gmail.com>
2020-12-27 15:19:42 +0700

    changed uri in client for deploy

--- client/src/config/graphql.js
+++ client/src/config/graphql.js
@@ -2,7 +2,8 @@ import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
 import {GET_PREMI} from '../query/'
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  // uri: 'http://localhost:4000',
+  uri: 'http://18.141.202.237:4000',
   cache: new InMemoryCache()
 })
 
