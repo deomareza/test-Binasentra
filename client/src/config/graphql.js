f22a4b49b1cb10eb744281a4ed332c7660024bd9
Deo Ivan Mareza <deomareza@gmail.com>
2020-12-27 16:18:50 +0700

    1. revert back to localhost

--- client/src/config/graphql.js
+++ client/src/config/graphql.js
@@ -1,11 +1,9 @@
 import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
 import {GET_PREMI} from '../query/'
 
-// var uri = "mongodb+srv://admin:admin@cluster0.tndye.mongodb.net/insurance?retryWrites=true&w=majority"
-
 export const client = new ApolloClient({
-  // uri: 'http://localhost:4000',
-  uri: `http://18.141.202.237:4000`,
+  uri: 'http://localhost:4000',
+  // uri: `http://18.141.202.237:4000`,
   cache: new InMemoryCache()
 })
 
